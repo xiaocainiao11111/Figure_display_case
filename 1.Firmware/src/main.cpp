5493a6ff3947606923e9c0ca21e57dc8ee4c4373
cainiao <321489189@qq.com>
2025-07-20 02:59:34 +0800

    cainiao

--- 1.Firmware/src/main.cpp
+++ 1.Firmware/src/main.cpp
@@ -2,13 +2,19 @@
 #include <Adafruit_NeoPixel.h>
 #include <main.h>
 
-
 #define POWER_CLOCK 26
 #define ESP32_RUNNING_CORE 1 // 内核选择   LVGL用核心0  其他用核心1
 
-TaskHandle_t Task_start_Handle; // 初始任务
+TaskHandle_t Task_start_Handle;  // 初始任务
+TaskHandle_t Task_ws2812_Handle; // ws2812 任务
+TaskHandle_t Task_key_Handle;  // 初始任务
 
-QueueHandle_t start_Queue; // 消息队列
+QueueHandle_t start_Queue;       // 消息队列
+QueueHandle_t iot_control_Queue; // ws2812 接收消息队列
+QueueHandle_t key_Queue; // ws2812 接收消息队列
+
+
+_ws2812_message WS2812_MSG;
 
 void Task_start(void *pvParameters)
 {
@@ -16,7 +22,21 @@ void Task_start(void *pvParameters)
     digitalWrite(POWER_CLOCK, HIGH);
     while (1)
     {
+        // __attachInterruptFunctionalArg();
         /* code */
+        Serial.println("12345");
+        vTaskDelay(1000);
+    }
+}
+
+void Task_ws2812(void *pvParameters)
+{
+    // pinMode(POWER_CLOCK, OUTPUT);
+    // digitalWrite(POWER_CLOCK, HIGH);
+    while (1)
+    {
+        /* code */
+        vTaskDelay(1000);
     }
 }
 
@@ -25,13 +45,14 @@ void setup()
     // pinMode(POWER_CLOCK, OUTPUT);
     // digitalWrite(POWER_CLOCK, LOW);
     Serial.begin(115200);
-    Serial1.begin(115200);
 
     xTaskCreatePinnedToCore(
         Task_start, "start_task", 4096, NULL, 2, &Task_start_Handle, ESP32_RUNNING_CORE);
+    xTaskCreatePinnedToCore(
+        Task_ws2812, "Task_ws2812", 1024, NULL, 4, &Task_ws2812_Handle, ESP32_RUNNING_CORE);
 }
 
 void loop()
 {
-    delay(10000);
+    // delay(10000);
 }
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # figure_display_case
 手办展示盒
+
+开发环境：
+
+
+
+功能：
+
+
+
+
+参考工程：
+https://gitee.com/wenzhengclub/super_knob
+
+https://github.com/xiaocainiao11111/ESP32_connect_XboxController
+
+https://github.com/unlir/XDrive
+
+https://github.com/peng-zhihui/Peak
